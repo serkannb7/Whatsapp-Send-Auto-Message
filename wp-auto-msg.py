cb62dd79d1189e9af7b90311bf434a85ddea9143
Serkan <122411694+serkannb7@users.noreply.github.com>
2023-03-13 16:10:39 +0300

    Update wp-auto-msg.py

--- wp-auto-msg.py
+++ wp-auto-msg.py
@@ -2,11 +2,11 @@ import pywhatkit
 import pyautogui
 
 try:
-    telefon_no = input("Telefon Numarasını Girin...\n" "(Başına '+90' eklemeyi unutmayın !!! )\n")
-    mesaj = input("Mesajınızı Girin...\n")
+    TELEFON_NO = input("Telefon Numarasını Girin...\n" "(Başına '+90' eklemeyi unutmayın !!! )\n")
+    MESAJ = input("Mesajınızı Girin...\n")
     SAAT = int(input("Saati Girin...\n"))
     DAKIKA = int(input("Dakika Girin....\n"))
-    pywhatkit.sendwhatmsg(telefon_no,mesaj,SAAT,DAKIKA,40)
+    pywhatkit.sendwhatmsg(TELEFON_NO,MESAJ,SAAT,DAKIKA,40)
     pyautogui.press('enter')
     print("Mesaj GÖnderildi")
 except:
